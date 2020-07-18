48a41c32b9a92ce26838b25179568fcc8d5a6e79
ferologics <f.hetes@gmail.com>
2020-07-18 12:48:50 +0200

    Test file conversion with pthread

--- Sources/Convertor/Convertor.swift
+++ Sources/Convertor/Convertor.swift
@@ -1,3 +1,136 @@
-struct Convertor {
-    var text = "Hello, World!"
+import FileKit
+import Foundation
+
+@available(iOS 10.0, *)
+public class Convertor {
+    
+    // MARK: Private Properties
+    
+    private var conversionTimers: [String:Timer] = [:]
+    private lazy var lock: pthread_rwlock_t = {
+        var lock = pthread_rwlock_t()
+        pthread_rwlock_init(&lock, nil)
+        return lock
+    }()
+    
+    // MARK: Public Properties
+    
+    public var delegate: ConversionDelegate?
+    
+    
+    // MARK: Life cycle
+    
+    public convenience init(delegate: ConversionDelegate) {
+        self.init()
+        self.delegate = delegate
+    }
+    
+    deinit {
+        pthread_rwlock_rdlock(&lock)
+        for timer in conversionTimers.values {
+            timer.invalidate()
+        }
+        pthread_rwlock_unlock(&lock)
+        pthread_rwlock_destroy(&lock)
+    }
+    
+    // MARK: - Public API
+    
+    public func convert(file: File<Data>, to format: OutputFormat) throws {
+        guard file.pathExtension == InputFormat.shapr.rawValue else {
+            throw Error.invalidInputFormat(of: file)
+        }
+        
+        print("read lock:", pthread_rwlock_tryrdlock(&lock))
+        guard !conversionTimers.keys.contains(file.name) else {
+            print("unlock:", pthread_rwlock_unlock(&lock))
+            throw Error.conversionAlreadyInProgress(of: file)
+        }
+        print("unlock:", pthread_rwlock_unlock(&lock))
+        
+        internalConvert(file: file, to: format)
+    }
+    
+    public func convert(files: [File<Data>], to format: OutputFormat) throws {
+        for file in files {
+            try convert(file: file, to: format)
+        }
+    }
+    
+    // MARK: - Internal API
+    
+    func internalConvert(file: File<Data>, to format: OutputFormat) {
+        // set up state
+        var progress = 0
+        let duration = Int.random(in: 5...25)
+        // schedule timer
+        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            progress += 1
+            if progress == duration {
+                timer.invalidate()
+                self.finishConversion(of: file, to: format)
+            } else {
+                let value = Double(progress)/Double(duration)
+                self.delegate?.didUpdateProgress(of: file, to: value)
+            }
+        }
+ 
+        
+        let error = pthread_rwlock_trywrlock(&lock)
+        if error == 0 {
+            conversionTimers[file.name] = timer
+            print("unlock:", pthread_rwlock_unlock(&lock))
+        } else if error == EDEADLK {
+            conversionTimers[file.name] = timer
+            print("unlock:", pthread_rwlock_unlock(&lock))
+        } else {
+            // TODO: wait until we have lock
+            let error = pthread_rwlock_trywrlock(&lock)
+            conversionTimers[file.name] = timer
+            print("unlock:", pthread_rwlock_unlock(&lock))
+            print("resource busy error:", error)
+        }
+        
+        // print("unlock:", pthread_rwlock_unlock(&lock))
+        RunLoop.current.run()
+    }
+    
+    func finishConversion(of file: File<Data>, to format: OutputFormat) {
+        let convertedFile = File<Data>(path: file.path)
+        convertedFile.pathExtension = format.rawValue
+        delegate?.didConvert(file: file, to: convertedFile)
+        print("write lock:", pthread_rwlock_trywrlock(&lock))
+        conversionTimers.removeValue(forKey: file.name)
+        print("unlock:", pthread_rwlock_unlock(&lock))
+    }
+}
+
+// MARK: - Supported Formats
+
+@available(iOS 10.0, *)
+extension Convertor {
+    public enum InputFormat: String {
+        case shapr
+    }
+    
+    public enum OutputFormat: String {
+        case step, stl, obj
+    }
+}
+
+// MARK: - Error handling
+
+@available(iOS 10.0, *)
+extension Convertor {
+    enum Error: Swift.Error {
+        case invalidInputFormat(of: File<Data>)
+        case conversionAlreadyInProgress(of: File<Data>)
+    }
+}
+
+// MARK: - Delegate
+
+public protocol ConversionDelegate {
+    func didConvert(file: File<Data>, to convertedFile: File<Data>)
+    func didUpdateProgress(of file: File<Data>, to value: Double)
 }
--- Tests/ConvertorTests/ConvertorTests.swift
+++ Tests/ConvertorTests/ConvertorTests.swift
@@ -1,15 +1,108 @@
 import XCTest
+import FileKit
+
 @testable import Convertor
 
+@available(iOS 10.0, *)
 final class ConvertorTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Convertor().text, "Hello, World!")
+    
+    // MARK: Properties
+    
+    private lazy var convertor = Convertor(delegate: self)
+    private var activeConversions: [String:Convertor.OutputFormat] = [:]
+    private let queue = DispatchQueue(label: "ConversionTestQueue", attributes: .concurrent)
+    private lazy var lock = pthread_rwlock_t()
+    private lazy var expectation = XCTestExpectation()
+    
+    // MARK: Life cycle
+    
+    override func setUpWithError() throws {
+        pthread_rwlock_init(&lock, nil)
+    }
+    
+    override func tearDownWithError() throws {
+        activeConversions = [:]
+        pthread_rwlock_destroy(&lock)
+    }
+    
+    // MARK: - Tests
+    
+    func testConvertFile() {
+        // GIVEN
+        let fileName = "test"
+        let filePath = Path(fileName + ".shapr")
+        let file = File<Data>(path: filePath)
+        let format: Convertor.OutputFormat = .obj
+        
+        // WHEN
+        self.activeConversions[file.name] = format
+        queue.async {
+            try! self.convertor.convert(file: file, to: format)
+        }
+        
+        // THEN
+        wait(for: [expectation], timeout: 30)
+    }
+    
+    func testConvertFiles() {
+        // GIVEN
+        let fileNames = ["test", "no", "test", "me"]
+        let filePaths = fileNames.map { Path($0 + ".shapr") }
+        let files = filePaths.map { File<Data>(path: $0) }
+        let format: Convertor.OutputFormat = .obj
+        expectation.expectedFulfillmentCount = files.count
+        
+        // WHEN
+        for file in files {
+            self.activeConversions[file.name] = format
+            queue.async {
+                try! self.convertor.convert(file: file, to: format)
+            }
+        }
+        
+        // THEN
+        wait(for: [expectation], timeout: 30)
+    }
+    
+    func testFailConvertFile() {
+        // TODO: test convert file
+    }
+    
+    func testFailConvertFiles() {
+        // TODO: test convert file
+    }
+    
+    func testProgressUpdate() {
+        
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testConvertFile", testConvertFile),
+        ("testConvertFiles", testConvertFiles),
+        ("testFailConvertFile", testFailConvertFile),
+        ("testFailConvertFiles", testFailConvertFiles),
+        ("testProgressUpdate", testProgressUpdate),
     ]
+}
+
+@available(iOS 10.0, *)
+extension ConvertorTests: ConversionDelegate {
+    func didUpdateProgress(of file: File<Data>, to value: Double) {
+        print("Progress of file '\(file.name)':  \(value)% ...")
+    }
+    
+    func didConvert(file: File<Data>, to convertedFile: File<Data>) {
+        pthread_rwlock_tryrdlock(&lock)
+        if let format = activeConversions[file.name] {
+            pthread_rwlock_unlock(&lock)
+            XCTAssertEqual(format.rawValue, convertedFile.pathExtension)
+            expectation.fulfill()
+        } else {
+            pthread_rwlock_unlock(&lock)
+        }
+    }
+    
+    func didFailToConvert(file: File<Data>, with error: Error) {
+        
+    }
 }
